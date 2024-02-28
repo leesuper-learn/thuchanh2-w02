674750d008c7f5852d19c113533468602ddf8c14
leesuper-learn <161559713+leesuper-learn@users.noreply.github.com>
2024-02-28 16:39:33 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luyentapsudungtoantu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten tu ban phim: ");
+            string str_name = Console.ReadLine();
+            Console.WriteLine("Xin chao " + str_name);
+
+            Console.ReadKey();
+        }
+    }
+}
